c3bb29473087e8b64a4526b5b3b4a2452644da84
f0rsakeN-afk <nar3sh0@gmail.com>
2025-06-20 22:23:04 +0545

    added zod validation

--- src/validators/auth.ts
+++ src/validators/auth.ts
@@ -10,3 +10,17 @@ export const loginSchema = z.object({
   email: z.string().email(),
   password: z.string().min(6),
 });
+
+export const forgotPasswordSchema = z.object({
+  email: z.string().email(),
+});
+
+export const resetPasswordSchema = z.object({
+  token: z.string(),
+  newPassword: z.string().min(6),
+});
+
+export const updatePasswordSchema = z.object({
+  currentPassword: z.string().min(6),
+  newPassword: z.string().min(6),
+});
