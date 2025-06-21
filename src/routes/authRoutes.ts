c26789016ed6e36c37e176fcda34b8141eb5acec
f0rsakeN-afk <nar3sh0@gmail.com>
2025-06-21 16:07:04 +0545

    added new routes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,20 @@
 import { Router, RequestHandler } from "express";
 import { login, register } from "../controllers/authController";
+import { updatePassword } from "../controllers/userCOntroller";
+import { protect } from "../middlewares/authMiddleware";
+import { forgotPassword } from "../controllers/passwordController";
+import { resetPassword } from "../controllers/passwordController";
 
 const router = Router();
 
 router.post("/register", register as RequestHandler);
 router.post("/login", login as RequestHandler);
+
+router.post("/resetPassword", resetPassword as RequestHandler);
+router.post("/forgotPassword", forgotPassword as RequestHandler);
+
+router.use(protect as RequestHandler);
+
+router.patch("/updatePassword", updatePassword as RequestHandler);
 
 export default router;
