858dbc581bed58ae422ad52c811588a736591108
f0rsakeN-afk <nar3sh0@gmail.com>
2025-06-22 09:02:47 +0545

    added routes for update and elete

--- src/routes/subRedditRoutes.ts
+++ src/routes/subRedditRoutes.ts
@@ -3,12 +3,20 @@ import { protect } from "../middlewares/authMiddleware";
 import {
   createSubReddit,
   getAllSubreddits,
+  updateSubreddit,
+  deleteSubreddit,
 } from "../controllers/subRedditController";
+import { isSubredditCreator } from "../middlewares/ownerShip";
 const router = Router();
 
 router.get("/", getAllSubreddits);
 
 router.use(protect as RequestHandler);
 router.post("/", createSubReddit as RequestHandler);
+
+router.use(isSubredditCreator as RequestHandler);
+
+router.patch("/:id", updateSubreddit as RequestHandler);
+router.delete("/:id", deleteSubreddit as RequestHandler);
 
 export default router;
