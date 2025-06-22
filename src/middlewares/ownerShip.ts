8e775390faebc4296c698db72b0befd61f1cba84
f0rsakeN-afk <nar3sh0@gmail.com>
2025-06-22 08:01:26 +0545

    added ownership middlewares for post and subreddit

---
+++ src/middlewares/ownerShip.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from "express";
+import prisma from "../lib/prisma";
+import { AuthRequest } from "./authMiddleware";
+
+export const isSubredditCreator = async (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+  const subreddit = await prisma.subreddit.findUnique({ where: { id } });
+
+  if (!subreddit || subreddit.creatorId !== req.userId)
+    return res.status(403).json({
+      error: "Not allowed to manage this subreddit",
+    });
+
+  next();
+};
+
+export const isPostOwner = async (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+  const post = await prisma.post.findUnique({ where: { id } });
+
+  if (!post || post.authorId !== req.userId)
+    return res.status(403).json({
+      error: "Not allowed to manage this post",
+    });
+
+  next();
+};
