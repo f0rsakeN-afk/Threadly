cfd2a0c38e83849a38714fc9c0d9df3a1e79459d
f0rsakeN-afk <nar3sh0@gmail.com>
2025-06-24 22:52:55 +0545

    Added comment owner middleware

--- src/middlewares/ownerShip.ts
+++ src/middlewares/ownerShip.ts
@@ -8,7 +8,7 @@ export const isSubredditCreator = async (
   next: NextFunction
 ) => {
   const { id } = req.params;
-/*   console.log(id) */
+  /*   console.log(id) */
   const subreddit = await prisma.subreddit.findUnique({ where: { id } });
 
   if (!subreddit || subreddit.creatorId !== req.userId)
@@ -30,6 +30,22 @@ export const isPostOwner = async (
   if (!post || post.authorId !== req.userId)
     return res.status(403).json({
       error: "Not allowed to manage this post",
+    });
+
+  next();
+};
+
+export const isCommentOwner = async (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+
+  const comment = await prisma.comment.findUnique({ where: { id } });
+  if (!comment || !(comment.authorId !== req.userId))
+    return res.status(403).json({
+      error: "Not allowed to manage this comment",
     });
 
   next();
