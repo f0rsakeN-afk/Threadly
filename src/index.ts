6ae37bfd90baa39b9301c48b3a006475e382dee2
f0rsakeN-afk <nar3sh0@gmail.com>
2025-06-25 08:39:33 +0545

    added new routes and middleware

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,20 @@ import authRoutes from "./routes/authRoutes";
 import postRoutes from "./routes/postRoutes";
 import subRedditRoutes from "./routes/subRedditRoutes";
 import commentRoutes from "./routes/commentRoutes";
+import voteRoutes from "./routes/voteRoutes";
+import { globalRateLimiter } from "./middlewares/rateLimiter";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
+app.use(globalRateLimiter);
 app.use("/api/subreddits", subRedditRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments/", commentRoutes);
+app.use("/api/votes", voteRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`🚀 Server running on http://localhost:${process.env.PORT}`);
